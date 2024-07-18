ce3049c197bfbc082f600f21de6a1245977cf39a
IceKane <bkgonzales157@gmail.com>
2024-07-19 04:39:51 +0800

    completed activity_dashboard.xml added workout_day.xml as a viewholder for activity_dashboard.xml:workoutDayRecycler added image assets button_blue.xml, button_gray.xml, status_complete.png, status_empty.png, and status_error.png, to be used for workout_day.xml added colors Gray, Light Gray to colors.xml added TODOs to Dashboard.kt in order to complete the recyclerview

--- app/src/main/java/com/mobdeve/fitmaster/Dashboard.kt
+++ app/src/main/java/com/mobdeve/fitmaster/Dashboard.kt
@@ -16,5 +16,13 @@ class Dashboard : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        /*TODO:
+           Implement data handler, data comes from firebase
+           Complete RecyclerView Adapter (id:workoutDayRecycler) using workout_day.xml as the viewholder layout.
+           Set imvStatus to status_(complete/error/empty).png depending on the workout status.
+           Set btnDay background to button_(gray/blue).xml if workout day is (completed/incomplete)
+           setonclicklistener on btnDay to start workout activity
+           Figure out how Manage Workout functions
+        */
     }
 }
