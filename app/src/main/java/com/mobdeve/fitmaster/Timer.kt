53c2cc98dfe24ae71681052dfe223bd141228b8b
houin679 <83742598+houin679@users.noreply.github.com>
2024-07-18 20:19:26 +0800

    Tried some stuff

--- app/src/main/java/com/mobdeve/fitmaster/Timer.kt
+++ app/src/main/java/com/mobdeve/fitmaster/Timer.kt
@@ -1,22 +1,72 @@
 package com.mobdeve.fitmaster
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.mobdeve.fitmaster.databinding.TimerBinding
 
 class Timer : AppCompatActivity() {
+    private lateinit var viewBinding: TimerBinding
+
+    private var timeInSeconds = 0
+    private var isRunning = false
+    private lateinit var handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.timer)
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        this.viewBinding = TimerBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        handler = Handler(Looper.getMainLooper())
+
+        viewBinding.btnStart.setOnClickListener {
+            startTimer()
         }
-         */
+
+        viewBinding.btnPause.setOnClickListener {
+            pauseTimer()
+        }
+
+        viewBinding.btnPause2.setOnClickListener {
+            resetTimer()
+        }
+    }
+
+    private fun startTimer() {
+        if (!isRunning) {
+            isRunning = true
+            handler.postDelayed(timerRunnable, 1000)
+        }
+    }
+
+    private fun pauseTimer() {
+        isRunning = false
+        handler.removeCallbacks(timerRunnable)
+    }
+
+    private fun resetTimer() {
+        isRunning = false
+        handler.removeCallbacks(timerRunnable)
+        timeInSeconds = 0
+        updateTimerDisplay()
+    }
+
+    private val timerRunnable = object : Runnable {
+        override fun run() {
+            if (isRunning) {
+                timeInSeconds++
+                updateTimerDisplay()
+                handler.postDelayed(this, 1000)
+            }
+        }
+    }
+
+    private fun updateTimerDisplay() {
+        val minutes = timeInSeconds / 60
+        val seconds = timeInSeconds % 60
+        viewBinding.tvwTime.text = String.format("%d:%02d", minutes, seconds)
     }
 }
