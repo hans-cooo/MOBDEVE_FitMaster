f2deab51222a92858f3e91a1602a7bc23a8df68a
IceKane <bkgonzales157@gmail.com>
2024-07-20 21:36:26 +0800

    implemented password change request

--- app/src/main/java/com/mobdeve/fitmaster/Profile.kt
+++ app/src/main/java/com/mobdeve/fitmaster/Profile.kt
@@ -2,6 +2,7 @@ package com.mobdeve.fitmaster
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import com.google.firebase.auth.FirebaseAuth
@@ -9,17 +10,31 @@ import com.mobdeve.fitmaster.databinding.ProfileBinding
 
 class Profile : ComponentActivity() {
     private lateinit var viewBinding: ProfileBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         this.viewBinding = ProfileBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+        auth = FirebaseAuth.getInstance()
 
         viewBinding.btnLogout.setOnClickListener(){
             FirebaseAuth.getInstance().signOut()
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
+        }
+
+        viewBinding.tvwChangePassword.setOnClickListener(){
+            if(auth.currentUser != null){
+                auth.sendPasswordResetEmail(auth.currentUser!!.email!!).addOnCompleteListener{ task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Reset link sent to your email", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this, "Unable to send reset mail", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
         }
     }
 }
