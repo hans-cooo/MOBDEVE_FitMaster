e28e0071d843d72ac189176d7e9d3194080e5f68
IceKane <bkgonzales157@gmail.com>
2024-07-20 18:52:20 +0800

    overwrote google-services.json with new configuration regenerated .idea and gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
