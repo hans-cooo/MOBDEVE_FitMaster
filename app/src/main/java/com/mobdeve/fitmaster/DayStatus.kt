dcfebdbf428405f60bb8efa982da8a89c18f1b02
hans-cooo <hans_coo@dlsu.edu.ph>
2024-07-24 12:03:17 +0800

    Fixed dashboard recycler view and connected firestore

---
+++ app/src/main/java/com/mobdeve/fitmaster/DayStatus.kt
@@ -0,0 +1,9 @@
+package com.mobdeve.fitmaster
+
+class DayStatus(day: String, status: Int) {
+    var day = day
+        private set
+
+    var status = status
+        private set
+}
--- app/src/main/java/com/mobdeve/fitmaster/MyFirestoreReferences.kt
+++ app/src/main/java/com/mobdeve/fitmaster/MyFirestoreReferences.kt
@@ -2,9 +2,19 @@ package com.mobdeve.fitmaster
 
 object MyFirestoreReferences {
     const val USERS_COLLECTION = "Users"
+    const val USER_PROGRESS_COLLECTION = "UserProgress"
 
     const val USERNAME_FIELD = "username"
     const val BIRTHDAY_FIELD = "birthday"
     const val EMAIL_FIELD = "email"
     const val PASSWORD_FIELD = "password"
+
+    const val DAY_1_FIELD = "Day1"
+    const val DAY_2_FIELD = "Day2"
+    const val DAY_3_FIELD = "Day3"
+    const val DAY_4_FIELD = "Day4"
+    const val DAY_5_FIELD = "Day5"
+    const val DAY_6_FIELD = "Day6"
+    const val DAY_7_FIELD = "Day7"
+
 }
