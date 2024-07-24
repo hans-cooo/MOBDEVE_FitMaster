e196fb72f208ae36673ae8835f1055f3f45d88ea
IceKane <bkgonzales157@gmail.com>
2024-07-24 15:48:40 +0800

    fixed unnamed recyclerview in edit_Workout

--- app/src/main/java/com/mobdeve/fitmaster/EditWorkout.kt
+++ app/src/main/java/com/mobdeve/fitmaster/EditWorkout.kt
@@ -5,16 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.mobdeve.fitmaster.databinding.ActivityDashboardBinding
+import com.mobdeve.fitmaster.databinding.EditWorkoutBinding
 
 class EditWorkout : AppCompatActivity() {
+    private lateinit var viewBinding: EditWorkoutBinding
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.viewBinding = EditWorkoutBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.edit_workout)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(viewBinding.root)
+
+        val email = this.intent.getStringExtra("email")
+        this.recyclerView = viewBinding.maxWeightRecycler
     }
 }
