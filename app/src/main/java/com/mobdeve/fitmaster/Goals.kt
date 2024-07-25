aea8b515327eb8e74d50893fcdc04db346776c3d
hans-cooo <hans_coo@dlsu.edu.ph>
2024-07-25 15:18:20 +0800

    Added button behavior in Goals.kt (Does not update db yet)

--- app/src/main/java/com/mobdeve/fitmaster/Goals.kt
+++ app/src/main/java/com/mobdeve/fitmaster/Goals.kt
@@ -15,8 +15,41 @@ class Goals : AppCompatActivity() {
         setContentView(viewBinding.root)
         val email = this.intent.getStringExtra("email")
 
+        viewBinding.btnBeginner.setOnClickListener {
+            if (viewBinding.btnBeginner.isChecked) {
+                viewBinding.btnIntermediate.isChecked = false
+                viewBinding.btnAdvanced.isChecked = false
+            }
+        }
+
+        viewBinding.btnIntermediate.setOnClickListener {
+            if (viewBinding.btnIntermediate.isChecked) {
+                viewBinding.btnBeginner.isChecked = false
+                viewBinding.btnAdvanced.isChecked = false
+            }
+        }
+
+        viewBinding.btnAdvanced.setOnClickListener {
+            if (viewBinding.btnAdvanced.isChecked) {
+                viewBinding.btnBeginner.isChecked = false
+                viewBinding.btnIntermediate.isChecked = false
+            }
+        }
+
+        viewBinding.btnWeightLoss.setOnClickListener {
+            if (viewBinding.btnWeightLoss.isChecked) {
+                viewBinding.btnGainMuscles.isChecked = false
+            }
+        }
+
+        viewBinding.btnGainMuscles.setOnClickListener {
+            if (viewBinding.btnGainMuscles.isChecked) {
+                viewBinding.btnWeightLoss.isChecked = false
+            }
+        }
+
         viewBinding.btnSave.setOnClickListener {
-            //TODO: Implement save feature
+
             val intent = Intent(this, Profile::class.java)
             intent.putExtra("email", email)
             startActivity(intent)
